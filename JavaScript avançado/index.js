cccde0da5fc3c7e67e4147ab136678a5c8d77b33
Gabriel Oliveira <118837823+Gabriel-Oliveiraa@users.noreply.github.com>
2024-02-15 16:09:57 -0300

    Add files via upload
    Destructuring & Spread Operator em Listas

--- JavaScript avançado/index.js
+++ JavaScript avançado/index.js
@@ -34,3 +34,20 @@ const age = 23;
 const city = 'Serra Talhada';
 
 const message6 = `Eu sou ${fullName}, tenho ${age} anos, e sou de ${city}!`
+
+//Destructuring em listas
+const numeros = [1,2,3,4];
+
+const [primeiro, segundo, terceiro, quarto] = numeros
+
+//const primeiro = numeros[0];
+//const segundo = numeros[1];
+//const terceiro = numeros[2];
+//const quarto = numeros[3];
+
+//Spread Operator em listas
+const numeros2 =[1,2,3];
+
+const numeroscom4e5 = [...numeros2, 4, 5];
+
+const [dez, vinte, ...resto] = [10,20,30,40,50]

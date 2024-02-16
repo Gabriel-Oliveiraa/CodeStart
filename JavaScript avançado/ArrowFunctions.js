d9fb4680f0420c0948154534d588e9eb672becfe
Gabriel Oliveira <118837823+Gabriel-Oliveiraa@users.noreply.github.com>
2024-02-15 21:02:57 -0300

    Add files via upload
    add arrow functions

---
+++ JavaScript avançado/ArrowFunctions.js
@@ -0,0 +1,19 @@
+//Function Normal
+function somar (a, b) {
+    return a + b
+}
+
+//Arrow function
+const somarArrow = (a, b) => {
+    return a + b
+}
+
+//Outra sintaxe de Arrow function
+const somarArrow2 = (a, b) => a + b
+
+//Exemplos
+function verificarPositivo (numero) {
+    return numero >= 0;
+}
+
+const verificarPositivoArrow = (numero) => numero >= 0;
